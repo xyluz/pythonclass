c7b19c57d19f502ab0bb719972939c5d0e668db5
lcarter505 <77297044+lcarter505@users.noreply.github.com>
2021-05-09 21:20:17 -0400

    Add files via upload

---
+++ Project/validation.py
@@ -0,0 +1,16 @@
+def account_number_validation(account_number):
+
+    if account_number:
+
+        try:
+            int(account_number)
+
+            if len(str(account_number)) == 10:
+                return True
+
+        except ValueError:
+            return False
+        except TypeError:
+            return False
+
+    return False
